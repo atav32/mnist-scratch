eace1a2e5dd01004b6bd53f30f3fcc8bbde07a18
Brian Zhang <brianbzhang@gmail.com>
2019-06-27 16:27:29 -0700

    add predictions

--- fashion_basic.py
+++ fashion_basic.py
@@ -14,6 +14,26 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 
 
 train_images, test_images = train_images / 255.0, test_images / 255.0
 
+model = keras.models.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax),
+])
+
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+model.fit(train_images, train_labels, epochs=3)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print('test accuracy:', test_acc)
+
+predictions = model.predict(test_images)
+
+
 # plt.figure()
 # plt.imshow(train_images[0])
 # plt.colorbar()
@@ -30,19 +50,67 @@ train_images, test_images = train_images / 255.0, test_images / 255.0
 #     plt.xlabel(class_names[train_labels[i]])
 # plt.show()
 
-model = keras.models.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),
-    keras.layers.Dense(128, activation=tf.nn.relu),
-    keras.layers.Dense(10, activation=tf.nn.softmax),
-])
 
-model.compile(
-    optimizer='adam',
-    loss='sparse_categorical_crossentropy',
-    metrics=['accuracy']
-)
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
 
-model.fit(train_images, train_labels, epochs=12)
+    plt.imshow(img, cmap=plt.cm.binary)
 
-test_loss, test_acc = model.evaluate(test_images, test_labels)
-print('test accuracy:', test_acc)
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel('{} {:2.0f}% ({})'.format(
+        class_names[predicted_label],
+        100 * np.max(predictions_array),
+        class_names[true_label],
+        color=color
+    ))
+
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color='#777777')
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+
+# Plot the first X test images, their predicted label, and the true label
+# Color correct predictions in blue, incorrect predictions in red
+def plot_predictions(predictions, test_labels):
+    num_rows = 5
+    num_cols = 3
+    num_images = num_rows * num_cols
+    plt.figure(figsize=(2 * 2 * num_cols, 2 * num_rows))
+
+    for i in range(num_images):
+        plt.subplot(num_rows, 2 * num_cols, 2 * i + 1)
+        plot_image(i, predictions, test_labels, test_images)
+        plt.subplot(num_rows, 2 * num_cols, 2 * i + 2)
+        plot_value_array(i, predictions, test_labels)
+    plt.show()
+
+
+def plot_single_prediction(model, index, test_images, test_labels, class_names):
+    img = test_images[0]
+    img = (np.expand_dims(img, 0))
+    predictions_single = model.predict(img)
+    plot_value_array(0, predictions_single, test_labels)
+    plt.xticks(range(10), class_names, rotation=45)
+    plt.show()
+
+
+# plot_single_prediction(model, 0, test_images, test_labels, class_names)
+
+plot_predictions(predictions, test_labels)
